1b429e5cd54ec40ac2ae2bedcc564804813c8a5e
skamprogiannis <boot.stefan.os@proton.me>
2026-02-08 17:09:06 +0200

    gen(3): migrate to flakes, home-manager, and hyprland

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Stefan's Cyberpunk NixOS";
+
+  inputs = {
+    # We use unstable to get the latest Hyprland and Ghostty
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.stefan = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
