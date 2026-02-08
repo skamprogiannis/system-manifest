e820838eaf1b600067536f42adc5b299d9b5dda9
skamprogiannis <boot.stefan.os@proton.me>
2026-02-08 17:09:07 +0200

    gen(55): update pearpass to use robust FHS environment

--- modules/pearpass.nix
+++ modules/pearpass.nix
@@ -11,84 +11,68 @@
     sha256 = "1fl5g4jb7k6y5j50cm8dfdib2kw31g4c0akz4svkbwf4szwlm1dn";
   };
 
-  pearpassApp = pkgs.appimageTools.wrapType2 {
-    pname = "pearpass";
-    version = "1.3.0";
-    src = pearpassSource;
-    extraPkgs = pkgs:
-      with pkgs; [
-        stdenv.cc.cc.lib
-        webkitgtk_6_0
-        gtk4
-        libadwaita
-        pango
-        cairo
-        gdk-pixbuf
-        glib
-        graphene
-        libsoup_2_4
-        libsoup_3
-        libsecret
-        icu
-        libGL
-        libglvnd
-        vulkan-loader
-        libayatana-appindicator
-        libnotify
-        udev
-        alsa-lib
-        nss
-        nspr
-        at-spi2-atk
-        libdrm
-        mesa
-        libxkbcommon
-        wayland
-        libxshmfence
-        libX11
-        libXcursor
-        libXdamage
-        libXext
-        libXfixes
-        libXi
-        libXrender
-        libXtst
-        libXcomposite
-        libXrandr
-        libXScrnSaver
-        gst_all_1.gstreamer
-        gst_all_1.gst-plugins-base
-        gst_all_1.gst-plugins-good
-        gst_all_1.gst-plugins-bad
-        libva
-        libvdpau
-        # Added for stability
-        dbus
-        dconf
-        xdg-utils
-        libz
-        openssl
-        gnome-themes-extra
-        gtk3
-        glib-networking
-        at-spi2-core
-        # Added for tray support / potential missing deps
-        libdbusmenu-gtk3
-        libappindicator-gtk3
-        # Tools
-        strace
-      ];
-  };
-
+  # Extract the AppImage to get the icon and resources
   pearpassExtracted = pkgs.appimageTools.extract {
     pname = "pearpass";
     version = "1.3.0";
     src = pearpassSource;
   };
 
-  # Modified wrapper to use strace for debugging silent exits
+  # Create an FHS environment for PearPass
+  # This mimics a standard Linux filesystem (Ubuntu/Debian) which proprietary apps expect
+  pearpassFHS = pkgs.appimageTools.wrapType2 {
+    pname = "pearpass";
+    version = "1.3.0";
+    src = pearpassSource;
+    extraPkgs = pkgs:
+      with pkgs; [
+        # Base libraries
+        glibc
+        glib
+        gcc-unwrapped
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrender
+        xorg.libXtst
+        xorg.libXcomposite
+        xorg.libXrandr
+        xorg.libXScrnSaver
+        nss
+        nspr
+        dbus
+        atk
+        at-spi2-atk
+        at-spi2-core
+        cups
+        expat
+        libdrm
+        libxkbcommon
+        mesa
+        alsa-lib
+        cairo
+        pango
+        gtk3
+        gtk4
+        gdk-pixbuf
+        libsecret
+        libnotify
+        libappindicator-gtk3
+        libdbusmenu-gtk3
+        systemd
+        udev
+        vulkan-loader
+        xdg-utils
+        zlib
+      ];
+  };
+
+  # Wrapper for Native Messaging (headless mode)
   pearpassNativeWrapper = pkgs.writeShellScript "pearpass-native" ''
-    exec ${pkgs.strace}/bin/strace -f -o /tmp/pearpass-strace.log ${pearpassApp}/bin/pearpass run --trusted pear://rdy3nr56u7k13dppa3sirj4qk3kfz6k7sss6zms3m5rspwr9wery "$@"
+    exec ${pearpassFHS}/bin/pearpass run --trusted pear://rdy3nr56u7k13dppa3sirj4qk3kfz6k7sss6zms3m5rspwr9wery "$@"
   '';
 
   pearpassManifest = pkgs.writeText "pearpass-manifest.json" (builtins.toJSON {
@@ -101,7 +85,7 @@
     ];
   });
 in {
-  home.packages = [pearpassApp];
+  home.packages = [pearpassFHS];
 
   xdg.desktopEntries.pearpass = {
     name = "PearPass";
