a03f50cd7834840780b3e636d015e7258c39702d
skamprogiannis <boot.stefan.os@proton.me>
2026-02-10 10:18:07 +0200

    fix(system): remove built-in console, fix pearpass libsoup, refine brave policy
    Original-Generation: gen(82)

--- modules/git.nix
+++ modules/git.nix
@@ -5,7 +5,6 @@
 }: {
   programs.git = {
     enable = true;
-    # Using 'settings' structure as recommended by recent Home Manager versions
     settings = {
       user = {
         name = "skamprogiannis";
@@ -13,9 +12,40 @@
       };
       core = {
         editor = "nvim";
+        sshCommand = "ssh -o AddKeysToAgent=yes";
       };
       credential = {
-        helper = "${pkgs.gh}/bin/gh auth git-credential";
+        helper = "";
+      };
+      url = {
+        "git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+        "git@platform.zone01.gr:" = {
+          insteadOf = "https://platform.zone01.gr/";
+        };
+      };
+    };
+  };
+
+  # Enable ssh-agent service for persistent authentication
+  services.ssh-agent.enable = true;
+
+  # SSH configuration for multi-host support
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        host = "github.com";
+        identityFile = "~/.ssh/id_ed25519_github";
+        addKeysToAgent = "yes";
+        identitiesOnly = true;
+      };
+      "platform.zone01.gr" = {
+        host = "platform.zone01.gr";
+        identityFile = "~/.ssh/id_ed25519_gitea";
+        addKeysToAgent = "yes";
+        identitiesOnly = true;
       };
     };
   };
