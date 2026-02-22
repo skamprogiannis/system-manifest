272d9f7f5d0dda7ced6f6348a2fd171786b4b8a1
skamprogiannis <boot.stefan.os@proton.me>
2026-02-22 18:03:12 +0200

    feat(home): set Loupe as default image viewer

--- home.nix
+++ home.nix
@@ -203,6 +203,13 @@
     defaultApplications = {
       "x-scheme-handler/magnet" = ["tremc.desktop"];
       "application/x-bittorrent" = ["tremc.desktop"];
+      "image/png" = ["org.gnome.Loupe.desktop"];
+      "image/jpeg" = ["org.gnome.Loupe.desktop"];
+      "image/gif" = ["org.gnome.Loupe.desktop"];
+      "image/webp" = ["org.gnome.Loupe.desktop"];
+      "image/bmp" = ["org.gnome.Loupe.desktop"];
+      "image/tiff" = ["org.gnome.Loupe.desktop"];
+      "image/svg+xml" = ["org.gnome.Loupe.desktop"];
     };
   };
 }
