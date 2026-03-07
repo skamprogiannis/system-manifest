76d37b1d8dac3f98c37b13b783b389ef2cae3bd1
skamprogiannis <boot.stefan.os@proton.me>
2026-03-07 17:16:22 +0200

    fix(keyring): enable gnome-keyring PAM unlock for gdm sessions

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -107,6 +107,8 @@
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services.gdm-password.enableGnomeKeyring = true;
 
   # Enable sound with pipewire
   services.pulseaudio.enable = false;
