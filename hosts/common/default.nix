ee851843e1b27e5f91485b2bd8d451dd5c5597b2
skamprogiannis <boot.stefan.os@proton.me>
2026-03-29 13:51:37 +0300

    fix(common): quiet greetd PAM session messages

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -109,6 +109,7 @@
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
+  security.pam.services.greetd.rules.session.unix.settings.quiet = true;
 
   # Enable sound with pipewire
   services.pulseaudio.enable = false;
