e3ef0f6c52ee326b9aece34ed6be1e31fc745b27
skamprogiannis <boot.stefan.os@proton.me>
2026-06-12 12:09:28 +0300

    fix(home): use stock hazy spotify theme

--- modules/home/spicetify.nix
+++ modules/home/spicetify.nix
@@ -7,19 +7,15 @@
 in {
   programs.spicetify = {
     enable = true;
-    theme =
-      spicePkgs.themes.hazy
-      // {
-        additionalCss = builtins.readFile ./spicetify/hazy-system-manifest.css;
-      };
-    customColorScheme = {
-      main = "0A0A0A";
-      subtext = "E3E2E8";
-      shadow = "000000";
-      text = "F8F7FF";
-      button = "B0C6FF";
-      button-active = "C9D0FF";
-      accent = "B0C6FF";
-    };
+    theme = spicePkgs.themes.hazy;
+    extraCommands = ''
+          substituteInPlace Extensions/hazy.js \
+            --replace-fail \
+              '  const defImage = "https://i.imgur.com/Wl2D0h0.png";' \
+              '  if (window.__systemManifestHazyLoaded) return;
+      window.__systemManifestHazyLoaded = true;
+
+      const defImage = "https://i.imgur.com/Wl2D0h0.png";'
+    '';
   };
 }
