792aaee6641d8a12c249cbf3b8f173750a602b44
skamprogiannis <boot.stefan.os@proton.me>
2026-07-24 12:42:09 +0300

    fix(firefox): restore legacy profile path

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   programs.firefox = {
     enable = true;
-    configPath = "${config.xdg.configHome}/mozilla/firefox";
+    configPath = "${config.home.homeDirectory}/.mozilla/firefox";
     profiles.stefan = {
       isDefault = true;
       settings = {
